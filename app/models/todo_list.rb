9c506e7938b37449143130963f54f8b685f4559a
Xander Miller <xander.miller@gmail.com>
2016-07-12 21:50:50 +0000

    adds validations to todo_list description

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,5 +3,8 @@ class TodoList < ActiveRecord::Base
   #Validations
   validates :title, presence: true,
                     length: { minimum: 3 }
+                    
+  validates :description, presence: true,
+                          length: { minimum: 5 }
 
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe TodoList, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_length_of(:title).is_at_least(3) }
+    it { should validate_presence_of(:description) }
+    it { should validate_length_of(:description).is_at_least(5) }
   end
 
   describe "scopes" do
