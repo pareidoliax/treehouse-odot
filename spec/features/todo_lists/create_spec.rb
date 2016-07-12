758ca2446b2db7752f8f8a8680a9af66e81d0e8a
Xander Miller <xander.miller@gmail.com>
2016-07-12 21:10:10 +0000

    completes create spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -5,5 +5,10 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     click_link "New Todo list"
     expect(page).to have_content 'New Todo List'
+    fill_in 'Title', with: 'My Grocery List'
+    fill_in 'Description', with: 'Groceries to buy this week.'
+    click_button 'Create Todo list'
+    expect(page).to have_content 'Todo list was successfully created.'
+    expect(page).to have_content 'Title: My Grocery List'
   end
 end
